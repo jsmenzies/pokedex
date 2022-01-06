1f336a20c89af01ac8dbda1416ed4b68e442c7bf
James Menzies <james@menzies.io>
2022-01-06 15:23:49 -0600

    Migrated page to params in load function

--- src/routes/pokemon/[id].svelte
+++ src/routes/pokemon/[id].svelte
@@ -1,6 +1,7 @@
 <script context='module'>
-	export async function load({ page }) {
-		const id = page.params.id;
+	export async function load({ params }) {
+		console.log(params)
+		const id = params.id;
 		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 		const res = await fetch(url);
 		const pokemon = await res.json();
